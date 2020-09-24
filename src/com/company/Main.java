fd66c01e65a8e13721e84d478ee362b056dfab27
th065973 <thomas.hanahoe@cerner.com>
2020-09-24 18:37:48 +0100

    Answer Part a and b Of Question 2
    Create printArray Method
    Remove some bugs

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,14 +1,14 @@
 //Look Up: http://best-practice-software-engineering.ifs.tuwien.ac.at/patterns/factory.html
 package com.company;
 
-public final class Main {
+public class Main {
 
-    private Main() {
-    }
+//    private Main() {
+//    }
 
-    public static Main createMain() {
-        return new Main();
-    }
+//    public static Main createMain() {
+//        return new Main();
+//    }
 
     public static void main(String[] args) {
         start();
@@ -29,7 +29,7 @@ public final class Main {
             a. Swap the first and last elements in the array.
             b. Shift all elements by one to the right and move the last element into the first
             position. For example, 1 4 9 16 25 would be transformed into 25 1 4 9 16.
-            c.  Replace all even elements with 0.
+            c. Replace all even elements with 0.
             d. Replace each element except the first and last by the larger of its two neighbors.
             programming exercises 307 e. Remove the middle element if the array length is odd, or the middle two
             elements if the length is even.
@@ -40,6 +40,35 @@ public final class Main {
             j.  Return true if the array contains duplicate elements (which need not be adjacent).
         * */
 
+        int[] array = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+        q2SwapFirstAndLast(array);
+        q2Shift1ToTheRight(array);
+    }
+
+    private static void q2Shift1ToTheRight(int[] array) {
+        // Shift all elements by one to the right and move the last element into the first
+
+        int last = array[array.length-1];
+        for(int i = array.length-1; i >= 1; i--)
+        {
+            array[i] = array[i - 1];
+        }
+
+        array[0] = last;
+
+        printArray(array, "Shift one to the right");
+    }
+
+    private static void q2SwapFirstAndLast(int[] array) {
+        // Swap the first and last elements in the array.
+        int first = array[0];
+        int last = array[array.length-1];
+
+        array[0] = last;
+        array[array.length-1] = first;
+
+        printArray(array, "Swap");
 
     }
 
@@ -71,15 +100,21 @@ public final class Main {
             }
         }
 
-        System.out.println("");
+        System.out.println("\n");
 
         for(int i = array.length - 1; i > -1; i--)
         {
             System.out.print(array[i]);
         }
 
-        System.out.println("");
-        System.out.print(array[0]);
-        System.out.print(array[array.length - 1]);
+        System.out.print("\n" + array[0]);
+        System.out.println(array[array.length - 1]);
+    }
+
+    private static void printArray(int[] array, String arrayName) {
+        for (int i = 0; i <= array.length-1; i++)
+        {
+            System.out.println(arrayName + " Element [" + i + "]: " + array[i]);
+        }
     }
 }
